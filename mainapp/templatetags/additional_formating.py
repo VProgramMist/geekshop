0309e6bf44ea7d3c3df603aadc7c38e739507f18
VProgramMist <vvvaleriyyy@gmail.com>
2021-03-01 01:39:54 +1000

    Refactor text formatting function

--- mainapp/templatetags/additional_formating.py
+++ mainapp/templatetags/additional_formating.py
@@ -6,4 +6,4 @@ register = template.Library()
 @register.filter(name='pricing')
 def pricing(value):
     str_value = str(value)
-    return f"{str_value[:-6] + ' ' + str_value[-6:-3] + ',' + str_value[-2:] if value > 1000 else str_value[:-3] + ',' + str_value[-2:]}"
+    return str_value[:-6] + ' ' + str_value[-6:-3] + ',' + str_value[-2:] if value > 1000 else str_value[:-3] + ',' + str_value[-2:]
