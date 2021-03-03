d6c0a9f15077c718f56496aa5caf7d956b7cdc25
VProgramMist <vvvaleriyyy@gmail.com>
2021-03-03 22:33:10 +1000

    Add logout function

--- authapp/forms.py
+++ authapp/forms.py
@@ -13,3 +13,20 @@ class UserLoginForm(AuthenticationForm):
         self.fields['password'].widget.attrs['placeholder'] = 'Введите пароль'
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control py-4'
+
+
+class UserRegisterForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(UserRegisterForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Введите имя'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Введите фамилию'
+        self.fields['username'].widget.attrs['placeholder'] = 'Введите имя пользователя'
+        self.fields['email'].widget.attrs['placeholder'] = 'Введите адрес эл. почты'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Введите пароль'
+        self.fields['password2'].widget.attrs['placeholder'] = 'подтвердите пароль'
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control py-4'
--- authapp/urls.py
+++ authapp/urls.py
@@ -7,4 +7,5 @@ app_name = 'authapp'
 urlpatterns = [
    path('login/', authapp.login, name='login'),
    path('register/', authapp.register, name='register'),
+   path('logout/', authapp.logout, name='logout'),
 ]
