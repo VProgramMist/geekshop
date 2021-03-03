86404b6c92efe2a6d2c7e003c7f7b6962de2fe0b
VProgramMist <vvvaleriyyy@gmail.com>
2021-03-03 22:07:01 +1000

    Add authorisation

---
+++ authapp/forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from authapp.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'password',)
+
+    def __init__(self, *args, **kwargs):
+        super(UserLoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Введите имя пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Введите пароль'
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control py-4'
